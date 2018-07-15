5b23d213ae7d4444d9e3d04aee01e8735e5f31d1
Alessandro <alessandro.spiercort@hotmail.com>
2018-07-15 10:09:58 -0300

    Alteração do tema da aplicação - Sandstone (Bootswatch)

---
+++ site/src/componentes/home_navbar.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
+} from 'reactstrap';
+
+export default class MenuNavegacao extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+          isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+          isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary My-Navbar" expand="md">
+                    <NavbarBrand href="/">Home</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="" navbar>
+                            <NavItem>
+                                <NavLink href="#">Components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="#">GitHub</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Options
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Option 1
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Option 2
+                                    </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        Reset
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
--- site/src/index.js
+++ site/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MenuNavegacao from './componentes/home_navbar';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<MenuNavegacao />, document.getElementById('root'));
 registerServiceWorker();
