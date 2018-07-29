a8875672c43f68edfd0b0db9098a85e785e69cf7
Alessandro Freitas <alessandro.spiercort@hotmail.com>
2018-07-29 10:40:16 -0300

    Teste de elemenstos no menu progressiva

--- site/src/componentes_home/progressiva.jsx
+++ site/src/componentes_home/progressiva.jsx
@@ -4,9 +4,14 @@ class Progressiva extends Component {
     render() {
         return (
             <div className="App">
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <div className="jumbotron">
+                    <h1>Teste Progressiva</h1>
+                </div>
             </div>        
         );
     }
