1a4fecebd6b778909dbf84786bc20051e730af80
Alessandro Freitas <alessandro.freitas@grupostudio.com.br>
2018-07-30 11:19:19 -0300

    Criação das pastas ref. os componentes da aplicação

--- site/src/index.js
+++ site/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
-import MenuNavegacao from './componentes_home/home_navbar';
-import Rodape from './componentes_home/home_footer';
-import Progressiva from './componentes_progressiva/progressiva';
+import MenuNavegacao from './componentes_main/home_navbar';
+import Rodape from './componentes_main/home_footer';
+import Progressiva from './menu_cabelos/componentes_progressiva/progressiva';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(<MenuNavegacao />, document.getElementById('root'));
